ed43c36cb1daa70ac0268d63602ddc9980ffb3af
edsat <esatar@me.com>
2023-02-05 19:46:33 +0100

    add check error message for short username and password

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -22,4 +22,14 @@ describe('Login Page Tests', function() {
       cy.get('[data-cy="link-login"]').should('be.visible');
       cy.get('[data-cy="text-field-ShowPassword-icon"]').should('be.visible');
   });
+
+  it('Check error message for short username', function() {
+    cy.get('#username').type('x');
+    cy.get('[data-cy="text-field-error-text"]').contains('Ihr Benutzername muss zwischen 2 und 64 Zeichen lang sein.').should('be.visible');
+  });
+
+  it('Check error message for short password', function() {
+    cy.get('#password').type('123');
+    cy.get('[data-cy="text-field-error-text"]').contains('Ihr Portal-Passwort muss zwischen 8 und 64 Zeichen lang sein.').should('be.visible');
+  });
 });
