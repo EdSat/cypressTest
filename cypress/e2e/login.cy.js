4d4beb296ec22bf88ef09b333a465a2a236e61bb
edsat <esatar@me.com>
2023-02-05 19:29:54 +0100

    add check login fields and buttons

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,25 @@
 describe('Login Page Tests', function() {
 
-  //Availability check 
-  //Skipping tests if not
   beforeEach(() => {
+    cy.visit('https://www.vodafone.de/business/business-portale/login.html')
+  })
+
+  it('Availability check', () => {
     cy.request('GET', 'https://www.vodafone.de/business/business-portale/login.html').then((response) => { 
       if (response.status < 200 || response.status >= 400) {  
         throw new Error('Login page is not available') 
       } 
-    }) 
-  })
-
-  it('Visits the login page', function() {
-    cy.visit('https://www.vodafone.de/business/business-portale/login.html')
-  })
-})
+    })
+  });
+  
+  it('Check login section input fields and buttons', () => {
+      cy.get('#username').should('be.visible');
+      cy.get('#password').should('be.visible');
+      cy.get('[data-cy="login"]').should('be.visible');
+      cy.get('[data-cy="register"]').should('be.visible');
+      cy.get('[data-cy="button-text-label icon-undefined"]').should('be.visible');
+      cy.get('[data-cy="link-information"]').should('be.visible');
+      cy.get('[data-cy="link-login"]').should('be.visible');
+      cy.get('[data-cy="text-field-ShowPassword-icon"]').should('be.visible');
+  });
+});
