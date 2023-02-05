abbe572f436e56257fbf01faecdc4a842e578a2d
edsat <esatar@me.com>
2023-02-05 02:17:07 +0100

    add availability check

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,15 @@
-describe('Login Test', function() {
+describe('Login Page Tests', function() {
+
+  //Availability check 
+  //Skipping tests if not
+  beforeEach(() => {
+    cy.request('GET', 'https://www.vodafone.de/business/business-portale/login.html').then((response) => { 
+      if (response.status < 200 || response.status >= 400) {  
+        throw new Error('Login page is not available') 
+      } 
+    }) 
+  })
+
   it('Visits the login page', function() {
     cy.visit('https://www.vodafone.de/business/business-portale/login.html')
   })
